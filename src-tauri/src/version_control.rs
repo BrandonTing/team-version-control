e0779b926010ba1c798acd47131fd6625189b508
Brandon Ting <brandon1221t@gmail.com>
2024-03-26 10:38:54 +0800

    experiment tauri store

--- src-tauri/src/version_control.rs
+++ src-tauri/src/version_control.rs
@@ -1,7 +1,9 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use specta::Type;
+use tauri_plugin_store::StoreBuilder;
 
-#[derive(Serialize, Type)]
+#[derive(Serialize, Type, Deserialize, Debug)]
 pub struct Team {
     pub title: String,
     pub description: String,
@@ -10,7 +12,7 @@ pub struct Team {
     pub current_branch: String,
 }
 
-#[derive(Serialize, Type)]
+#[derive(Serialize, Type, Deserialize, Debug)]
 pub struct Branch {
     pub id: String,
     pub title: String,
@@ -18,7 +20,7 @@ pub struct Branch {
     pub current_change: String,
 }
 
-#[derive(Serialize, Type)]
+#[derive(Serialize, Type, Deserialize, Debug)]
 pub struct Change {
     pub id: String,
     pub message: String,
@@ -27,12 +29,31 @@ pub struct Change {
 
 #[tauri::command]
 #[specta::specta] // <-- This bit here
-pub fn get_team() -> Vec<Team> {
-    vec![Team {
-        title: "test".to_string(),
-        description: "test".to_string(),
-        // created_at: time::SystemTime::now(),
-        branches: vec![],
-        current_branch: "".to_string(),
-    }]
+pub fn get_team(app_handle: tauri::AppHandle) -> Vec<Team> {
+    let mut store = StoreBuilder::new(app_handle, ".team.dat".parse().unwrap()).build();
+
+    let _ = store.load();
+    // let _ = store.insert(
+    //     "team1".to_string(),
+    //     json!(Team {
+    //         title: "test".to_string(),
+    //         description: "test".to_string(),
+    //         // created_at: time::SystemTime::now(),
+    //         branches: vec![],
+    //         current_branch: "".to_string(),
+    //     }),
+    // );
+    // match store.save() {
+    //     Err(e) => {
+    //         println!("{}", e);
+    //     }
+    //     _ => println!("saved"),
+    // };
+    match serde_json::from_value::<Vec<Team>>(store.values().map(|x| x.clone()).collect()) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{:?}", e);
+            vec![]
+        }
+    }
 }
